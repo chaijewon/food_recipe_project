014cd83672fbfda8bc2c7fdecc4dd45b3b6d99e4
chaijewon <vcandjava@nate.com>
2025-12-11 16:51:35 +0900

    1111

--- src/stores/index.js
+++ src/stores/index.js
@@ -1,6 +1,5 @@
 import { createStore } from "vuex";
 import foods from "./food"
-
 export default createStore({
     modules:{foods}
 })
--- src/views/main/HomeView.vue
+++ src/views/main/HomeView.vue
@@ -3,21 +3,21 @@
         <div class="row">
             <div class="col-md-3" v-for="(vo,index) in food_data.list" :key="index">
                 <div class="thumbnail">
-                    <a href="#">
+                    <router-link :to="{name:'food_detail',params:{fno:vo.fno}}">
                         <img :src="vo.poster" :title="vo.address" style="width:240px;height: 130px">
                         <div class="caption">
                             <p>{{vo.name}}</p>
                         </div>
-                    </a>
+                    </router-link>
                 </div>
             </div>
         </div>
         <div class="row text-center" style="margin-top: 20px">
             <ul class="pagination">
-                <li v-if="food_data.startPage > 1"><a class="a-link">&laquo;</a></li>
+                <li v-if="food_data.startPage > 1"><a class="a-link" @click="foodListData(food_data.startPage-1)">&laquo;</a></li>
                 <li v-for="i in range(food_data.startPage, food_data.endPage)" :key="i"
-                    :class="food_data.curpage == i ? 'active' : ''"><a class="a-link">{{i}}</a></li>
-                <li v-if="food_data.endPage < food_data.totalpage"><a class="a-link">&raquo;</a></li>
+                    :class="food_data.curpage == i ? 'active' : ''"><a class="a-link" @click="foodListData(i)">{{i}}</a></li>
+                <li v-if="food_data.endPage < food_data.totalpage"><a class="a-link" @click="foodListData(food_data.endPage+1)">&raquo;</a></li>
             </ul>
         </div>
     </div>
@@ -42,7 +42,8 @@
           |
         해당 vue에서 store에 있는 state를 읽어서 출력
     */
-   import { defineComponent,onMounted,computed } from 'vue';
+  
+import { defineComponent,onMounted,computed } from 'vue';
    import { useStore } from 'vuex';
    export default defineComponent({
       setup(){
@@ -86,5 +87,8 @@ p {
     overflow: hidden;
     white-space: nowrap;
     text-overflow: ellipsis;
+}
+.a-link:hover{
+    cursor: pointer;
 }
 </style>
